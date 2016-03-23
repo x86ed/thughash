d38865f66b60572bd201f553a035d2915db420ac
linksys <kitch bot>
2016-03-22 18:52:00 -0700

    gofmt

--- thughash.go
+++ thughash.go
@@ -1,24 +1,24 @@
 package thughash
 
-import(
+import (
 	"math"
 	"strconv"
 )
 
 func slicepos(slice []string, value string) float64 {
-    for p, v := range slice {
-        if (v == value) {
-            return float64(p)
-        }
-    }
-    return float64(-1)
+	for p, v := range slice {
+		if v == value {
+			return float64(p)
+		}
+	}
+	return float64(-1)
 }
 
-func slicerange(slice string, index int)int{
+func slicerange(slice string, index int) int {
 	var output int
 	output = index + 1
-	if output >= len(slice){
-		output = len(slice)-1
+	if output >= len(slice) {
+		output = len(slice) - 1
 	}
 	if output < 1 {
 		output = 0
@@ -26,26 +26,26 @@ func slicerange(slice string, index int)int{
 	return output
 }
 
-type ThugHash struct{
-	Index int
-	Subject string
-	Verb string
-	End string
+type ThugHash struct {
+	Index     int
+	Subject   string
+	Verb      string
+	End       string
 	Remainder int
 	QuickHash string
 }
 
 func (th *ThugHash) Generate(seed float64) {
 	var sm, vm float64
-	sm = math.Mod(seed,262144)
-	remainder := int64(seed/262144)
-	
-	vm = math.Mod(sm,4096)
-	subject := int64(sm/4096)
-	
-	end := int(math.Mod(vm,64))
-	verb := int64(vm/64)
-	
+	sm = math.Mod(seed, 262144)
+	remainder := int64(seed / 262144)
+
+	vm = math.Mod(sm, 4096)
+	subject := int64(sm / 4096)
+
+	end := int(math.Mod(vm, 64))
+	verb := int64(vm / 64)
+
 	th.Subject = Subjects[subject]
 	th.Verb = Verbs[verb]
 	th.End = Ends[end]
@@ -55,58 +55,58 @@ func (th *ThugHash) Generate(seed float64) {
 
 func (th *ThugHash) GenerateFrom(seed string) {
 	var seedNum float64
-	if len(seed) <= 7{
+	if len(seed) <= 7 {
 		seedInt, _ := strconv.ParseInt(seed, 32, 64)
 		seedNum = float64(seedInt)
-	} else{
+	} else {
 		seedNum = th.Degenerate(seed)
 	}
 	th.Generate(seedNum)
 }
 
-func (th *ThugHash) Degenerate(hash string) float64{
+func (th *ThugHash) Degenerate(hash string) float64 {
 	var output, pos float64
 	output = 0
 	pos = 0
 	mutantHash := hash
-		for s := range mutantHash{
-			testkey := hash[:s]
-			pos = slicepos(Subjects,testkey)
-			if pos > -1{
-				si := slicerange(mutantHash,s)
-				mutantHash = mutantHash[si:]
-				break
-			}
+	for s := range mutantHash {
+		testkey := hash[:s]
+		pos = slicepos(Subjects, testkey)
+		if pos > -1 {
+			si := slicerange(mutantHash, s)
+			mutantHash = mutantHash[si:]
+			break
 		}
-		output += pos*4096
-		for v := range mutantHash{
-			testkey := mutantHash[:v]
-			pos = slicepos(Verbs,testkey)
-			if pos > -1{
-				vi := slicerange(mutantHash,v)
-				mutantHash = mutantHash[vi:]
-				break
-			}
+	}
+	output += pos * 4096
+	for v := range mutantHash {
+		testkey := mutantHash[:v]
+		pos = slicepos(Verbs, testkey)
+		if pos > -1 {
+			vi := slicerange(mutantHash, v)
+			mutantHash = mutantHash[vi:]
+			break
 		}
-		output += pos*64
-		for e := range mutantHash{
-			testkey := mutantHash[:e]
-			pos = slicepos(Ends,testkey)
-			if pos > -1{
-				ei := slicerange(mutantHash,e)
-				mutantHash = mutantHash[ei:]
-				break
-			}
+	}
+	output += pos * 64
+	for e := range mutantHash {
+		testkey := mutantHash[:e]
+		pos = slicepos(Ends, testkey)
+		if pos > -1 {
+			ei := slicerange(mutantHash, e)
+			mutantHash = mutantHash[ei:]
+			break
 		}
-		output += pos
-		rem, _ := strconv.ParseFloat(mutantHash, 64)
-		rem -= 1000
-		rem = rem*262144
-		output += rem
+	}
+	output += pos
+	rem, _ := strconv.ParseFloat(mutantHash, 64)
+	rem -= 1000
+	rem = rem * 262144
+	output += rem
 	return output
 }
 
-func (th ThugHash) ThirtyTwoEncode(hash float64) string{
+func (th ThugHash) ThirtyTwoEncode(hash float64) string {
 	output := strconv.FormatInt(int64(hash), 32)
 	for len(output) < 7 {
 		output = "0" + output
@@ -114,20 +114,20 @@ func (th ThugHash) ThirtyTwoEncode(hash float64) string{
 	return output
 }
 
-func (th ThugHash) GenerateRemainder(raw int64) int{
+func (th ThugHash) GenerateRemainder(raw int64) int {
 	output := int(raw) + 1000
-	if output > 9999{
+	if output > 9999 {
 		output = 9999
 	}
 	return output
 }
 
-func (th ThugHash) MakeSlug() string{
-	output := th.Subject + "-" + th.Verb  + "-" +  th.End  + "-" +  strconv.Itoa(th.Remainder)
+func (th ThugHash) MakeSlug() string {
+	output := th.Subject + "-" + th.Verb + "-" + th.End + "-" + strconv.Itoa(th.Remainder)
 	return output
 }
 
-func (th *ThugHash) MatchHash(hash string) string{
+func (th *ThugHash) MatchHash(hash string) string {
 	var output string
 	if len(hash) <= 7 {
 		output = th.MakeSlug()
--- thughash_test.go
+++ thughash_test.go
@@ -1,75 +1,75 @@
 package thughash
 
-import(
+import (
 	"testing"
 )
 
-type testcase struct{
-	test float64
-	answer string 
-}
-
-type degenTest struct{
-	test string
-	answer float64
-}
-
-type genFrom struct{
-	test string
+type testcase struct {
+	test   float64
 	answer string
 }
 
-type thugTest struct{
-	test int64
-	subject string
-	verb string
-	end string
-	remainder int
-	quickhash string
-	answer string 
+type degenTest struct {
+	test   string
+	answer float64
 }
 
-type testInts struct{
-	test int64
+type genFrom struct {
+	test   string
+	answer string
+}
+
+type thugTest struct {
+	test      int64
+	subject   string
+	verb      string
+	end       string
+	remainder int
+	quickhash string
+	answer    string
+}
+
+type testInts struct {
+	test   int64
 	answer int
 }
 
 func TestThirtyTwoEncode(t *testing.T) {
-    t.Log("Testing the ThirtyTwoEncode method")
-    var th ThugHash
-    testArray := []testcase{
-    	testcase{15, "000000f"},
-    	testcase{999, "00000v7"},
-    	testcase{9999, "00009of"},
-    	testcase{0, "0000000"},
-    	testcase{2147483647, "1vvvvvv"},
-    	testcase{2147483648, "2000000"}}
-    for i := 0; i < len(testArray); i++ {
-	    x := th.ThirtyTwoEncode(testArray[i].test)
-	    if x != testArray[i].answer{
-	        t.Errorf("%d doesn't equal %q.", x, testArray[i].answer)
-	    }
+	t.Log("Testing the ThirtyTwoEncode method")
+	var th ThugHash
+	testArray := []testcase{
+		testcase{15, "000000f"},
+		testcase{999, "00000v7"},
+		testcase{9999, "00009of"},
+		testcase{0, "0000000"},
+		testcase{2147483647, "1vvvvvv"},
+		testcase{2147483648, "2000000"}}
+	for i := 0; i < len(testArray); i++ {
+		x := th.ThirtyTwoEncode(testArray[i].test)
+		if x != testArray[i].answer {
+			t.Errorf("%d doesn't equal %q.", x, testArray[i].answer)
+		}
 	}
 }
 
 func TestGenerate(t *testing.T) {
 	t.Log("Testing the Generate method")
 	testArray := []testcase{
-		testcase{0,"Baby-actin-a-double-cup-1000"},
-		testcase{227425999,"Mothafucka-turnt-holdin-it-down-1867"},
-    	testcase{15, "Baby-actin-holdin-it-down-1000"},
-    	testcase{999, "Baby-escapin-squad-1000"},
-    	testcase{9999, "Balla-pimpin-holdin-it-down-1000"},
-    	testcase{2147483647, "Weezy-woke-up-yo-ass-9191"},
-    	testcase{2147483648, "Baby-actin-a-double-cup-9192"},
-		testcase{247333,"Turnt-interceptin-skrilla-1000"}}
+		testcase{0, "Baby-actin-a-double-cup-1000"},
+		testcase{227425999, "Mothafucka-turnt-holdin-it-down-1867"},
+		testcase{15, "Baby-actin-holdin-it-down-1000"},
+		testcase{999, "Baby-escapin-squad-1000"},
+		testcase{9999, "Balla-pimpin-holdin-it-down-1000"},
+		testcase{2147483647, "Weezy-woke-up-yo-ass-9191"},
+		testcase{2147483648, "Baby-actin-a-double-cup-9192"},
+		testcase{247333, "Turnt-interceptin-skrilla-1000"}}
 	for i := 0; i < len(testArray); i++ {
-		var th ThugHash	
+		var th ThugHash
 		th.Generate(testArray[i].test)
-	    x := th.MakeSlug()
-	    if x != testArray[i].answer{
-	        t.Errorf("%q doesn't equal %q.", x, testArray[i].answer)
-	    }
+		x := th.MakeSlug()
+		if x != testArray[i].answer {
+			t.Errorf("%q doesn't equal %q.", x, testArray[i].answer)
+		}
 	}
 }
 
@@ -77,62 +77,62 @@ func TestGenerateRemainder(t *testing.T) {
 	t.Log("Testing the GenerateRemainder method")
 	var th ThugHash
 	testArray := []testInts{
-		testInts{1,1001},
-		testInts{9999,9999},
-		testInts{8999,9999},
-		testInts{404,1404},
-		testInts{0,1000}}
-    for i := 0; i < len(testArray); i++ {
-	    x := th.GenerateRemainder(testArray[i].test)
-	    if x != testArray[i].answer{
-	        t.Errorf("%d doesn't equal %d.", x, testArray[i].answer)
-	    }
-	}	
+		testInts{1, 1001},
+		testInts{9999, 9999},
+		testInts{8999, 9999},
+		testInts{404, 1404},
+		testInts{0, 1000}}
+	for i := 0; i < len(testArray); i++ {
+		x := th.GenerateRemainder(testArray[i].test)
+		if x != testArray[i].answer {
+			t.Errorf("%d doesn't equal %d.", x, testArray[i].answer)
+		}
+	}
 }
 
 func TestMakeSlug(t *testing.T) {
 	t.Log("Testing the MakeSlug method")
 	var th ThugHash
 	testArray := []thugTest{
-		thugTest{1,"OG","sippin","my-stash",1449,"sdfsdf","OG-sippin-my-stash-1449"},
-		thugTest{2,"Yayo","poppin","the-building",1926,"sdfsdf","Yayo-poppin-the-building-1926"},
-		thugTest{3,"Yayo","poppin","the-building",2005,"sdfsdf","Yayo-poppin-the-building-2005"}}
-    for i := 0; i < len(testArray); i++ {
-    	th.Index = int(testArray[i].test)
-    	th.Subject = testArray[i].subject
-    	th.Verb = testArray[i].verb
-    	th.End = testArray[i].end
-    	th.Remainder = testArray[i].remainder
-    	th.QuickHash = testArray[i].quickhash
-	    x := th.MakeSlug()
-	    if x != testArray[i].answer{
-	        t.Errorf("%q doesn't equal %q.", x, testArray[i].answer)
-	    }
-	}	
+		thugTest{1, "OG", "sippin", "my-stash", 1449, "sdfsdf", "OG-sippin-my-stash-1449"},
+		thugTest{2, "Yayo", "poppin", "the-building", 1926, "sdfsdf", "Yayo-poppin-the-building-1926"},
+		thugTest{3, "Yayo", "poppin", "the-building", 2005, "sdfsdf", "Yayo-poppin-the-building-2005"}}
+	for i := 0; i < len(testArray); i++ {
+		th.Index = int(testArray[i].test)
+		th.Subject = testArray[i].subject
+		th.Verb = testArray[i].verb
+		th.End = testArray[i].end
+		th.Remainder = testArray[i].remainder
+		th.QuickHash = testArray[i].quickhash
+		x := th.MakeSlug()
+		if x != testArray[i].answer {
+			t.Errorf("%q doesn't equal %q.", x, testArray[i].answer)
+		}
+	}
 }
 
 func TestMatchHash(t *testing.T) {
 	t.Log("Testing the MatchHash method")
 	var th ThugHash
 	testArray := []thugTest{
-		thugTest{1,"Balla","pimpin","holdin-it-down",1000,"00009of","Balla-pimpin-holdin-it-down-1000"},
-		thugTest{2,"Weezy","woke-up","yo-ass",9191,"1vvvvvv","Weezy-woke-up-yo-ass-9191"},
-		thugTest{3,"Baby","actin","a-double-cup",9192,"2000000","Baby-actin-a-double-cup-9192"}}
+		thugTest{1, "Balla", "pimpin", "holdin-it-down", 1000, "00009of", "Balla-pimpin-holdin-it-down-1000"},
+		thugTest{2, "Weezy", "woke-up", "yo-ass", 9191, "1vvvvvv", "Weezy-woke-up-yo-ass-9191"},
+		thugTest{3, "Baby", "actin", "a-double-cup", 9192, "2000000", "Baby-actin-a-double-cup-9192"}}
 	for i := 0; i < len(testArray); i++ {
 		th.Index = int(testArray[i].test)
-    	th.Subject = testArray[i].subject
-    	th.Verb = testArray[i].verb
-    	th.End = testArray[i].end
-    	th.Remainder = testArray[i].remainder
-    	th.QuickHash = testArray[i].quickhash
-	    x := th.MatchHash(th.MakeSlug())
-	    if x != testArray[i].quickhash{
-	        t.Errorf("%q doesn't equal %q.", x, testArray[i].quickhash)
-	    }
-	    y := th.MatchHash(th.QuickHash)
-	    if y != testArray[i].answer{
-	        t.Errorf("%q doesn't equal %q.", y, testArray[i].answer)
-	    }
+		th.Subject = testArray[i].subject
+		th.Verb = testArray[i].verb
+		th.End = testArray[i].end
+		th.Remainder = testArray[i].remainder
+		th.QuickHash = testArray[i].quickhash
+		x := th.MatchHash(th.MakeSlug())
+		if x != testArray[i].quickhash {
+			t.Errorf("%q doesn't equal %q.", x, testArray[i].quickhash)
+		}
+		y := th.MatchHash(th.QuickHash)
+		if y != testArray[i].answer {
+			t.Errorf("%q doesn't equal %q.", y, testArray[i].answer)
+		}
 	}
 }
 
@@ -140,29 +140,29 @@ func TestDegenerate(t *testing.T) {
 	t.Log("Testing the Degenerate method")
 	var th ThugHash
 	testArray := []degenTest{
-		degenTest{"Balla-pimpin-holdin-it-down-1000",9999},
-		degenTest{"Weezy-woke-up-yo-ass-9191",2147483647},
-		degenTest{"Yayo-poppin-the-building-2005",2.63718831e+08}}
+		degenTest{"Balla-pimpin-holdin-it-down-1000", 9999},
+		degenTest{"Weezy-woke-up-yo-ass-9191", 2147483647},
+		degenTest{"Yayo-poppin-the-building-2005", 2.63718831e+08}}
 	for i := 0; i < len(testArray); i++ {
 		x := th.Degenerate(testArray[i].test)
-		if x != testArray[i].answer{
-	        t.Errorf("%v doesn't equal %v.", x, testArray[i].answer)
-	    }
-	}	
+		if x != testArray[i].answer {
+			t.Errorf("%v doesn't equal %v.", x, testArray[i].answer)
+		}
+	}
 }
 
-func TestGenerateFrom(t *testing.T){
+func TestGenerateFrom(t *testing.T) {
 	t.Log("Testing the GenerateFrom method")
 	var th ThugHash
 	testArray := []genFrom{
-		genFrom{"1vvvvvv","1vvvvvv"},
-		genFrom{"0000000","0000000"},
-		genFrom{"Baby-actin-a-double-cup-9192","2000000"}}
+		genFrom{"1vvvvvv", "1vvvvvv"},
+		genFrom{"0000000", "0000000"},
+		genFrom{"Baby-actin-a-double-cup-9192", "2000000"}}
 	for i := 0; i < len(testArray); i++ {
 		th.GenerateFrom(testArray[i].test)
 		x := th.QuickHash
-		if x != testArray[i].answer{
-	        t.Errorf("%v doesn't equal %v.", x, testArray[i].answer)
-	    }
+		if x != testArray[i].answer {
+			t.Errorf("%v doesn't equal %v.", x, testArray[i].answer)
+		}
 	}
 }
