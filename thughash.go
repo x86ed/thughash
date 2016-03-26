335e704f48e890bee3791f21c700a0fd910deb41
x86ed <tobioatom@gmail.com>
2016-03-26 13:50:26 -0700

    gofmt

--- thughash.go
+++ thughash.go
@@ -3,8 +3,8 @@ package thughash
 import (
 	"math"
 	"strconv"
-    
-    "github.com/x86ed/thughash/words"
+
+	"github.com/x86ed/thughash/words"
 )
 
 func slicepos(slice []string, value string) float64 {
